41e87a02f97555f63b3092fb2192f185ac364754
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-02-28 20:26:07 +0000

    finished frontend and backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Query
 from transformers import pipeline
 import torch 
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
 
 pipe = pipeline("text-classification", model="lewisnjue/spam-detection")
+
+# Allow requests from your frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change "*" to specific origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def process_input(text: str = Query(...)):
